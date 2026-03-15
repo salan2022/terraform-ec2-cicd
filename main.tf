68b31a17c21199ccdbd9c0b57e3c92efe10aff4e
salan2022 <gulzarhadi4@gmail.com>
2026-03-15 01:27:57 +0000

    Add Terraform EC2 project with GitHub Actions workflow

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"               # Free-tier eligible
+  tags = {
+    Name = "terraform-cicd-ec2"
+  }
 }
