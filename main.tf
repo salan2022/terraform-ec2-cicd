811a1d8962d6be83b6f3e4d005b15205bd6881c1
salan2022 <gulzarhadi4@gmail.com>
2026-03-15 05:37:43 +0000

    Update EC2 Terraform configuration

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
-  instance_type = "t2.micro"               # Free-tier eligible
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
   tags = {
     Name = "terraform-cicd-ec2"
   }
